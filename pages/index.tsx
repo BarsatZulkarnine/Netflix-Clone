b47a9a1df1c84418bf1125a57f0ab7621c052bc5
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-02-16 20:58:18 +0600

    dont till scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import { Movie } from '../typings'
-import requests from '../utils/request'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Row from "../components/Row";
+import { Movie } from "../typings";
+import requests from "../utils/request";
 
 interface Props {
-  netflixOriginals: Movie[]
-  trendingNow: Movie[]
-  topRated: Movie[]
-  actionMovies: Movie[]
-  comedyMovies: Movie[]
-  horrorMovies: Movie[]
-  romanceMovies: Movie[]
-  documentaries: Movie[]
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
   //products: Product[]
 }
 
@@ -26,29 +27,36 @@ const Home = ({
   romanceMovies,
   topRated,
   trendingNow,
-  //products,
-}: Props)  => {
+}: //products,
+Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]
+    <div
+      className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]
      lg:h-[140vh]">
       <Head>
         <title>Netflix-clone</title>
         <link rel="stylesheet" href="" />
-       </Head>
-       <Header/>
+      </Head>
+      <Header />
 
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
+        <section>
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
 
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
       </main>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
 
 export const getServerSideProps = async () => {
   // const products = await getProducts(payments, {
@@ -76,7 +84,7 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+  ]);
 
   return {
     props: {
@@ -88,9 +96,7 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
-    //  products,
+      //  products,
     },
-  }
-
-
-}
+  };
+};
